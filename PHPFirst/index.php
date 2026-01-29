cf74d2587defc8567f9676a66e727efd312107fa
Matěj Holeček <st025840@student.cichnovabrno.cz>
2026-01-29 09:28:04 +0100

    Radio fix

--- PHPFirst/index.php
+++ PHPFirst/index.php
@@ -10,8 +10,8 @@
         <label for="text">TEXT: </label><input id="text" name="text" type="text">
         <label for="range">RANGE: </label><input id="range" name="range" type="range"><br>
         <label for="checkbox">CHECK: </label><input id="checkbox" name="checkbox" type="checkbox"><br>
-        <label for="radio1">RADIO 1:</label><input id="radio1" name="radio" type="radio">
-        <label for="radio2">RADIO 2:</label><input id="radio2" name="radio" type="radio">
+        <label for="radio1">RADIO 1:</label><input id="radio1" name="radio" type="radio" value="1">
+        <label for="radio2">RADIO 2:</label><input id="radio2" name="radio" type="radio" value="2">
         <input type="submit">
     </form>
  </body>
