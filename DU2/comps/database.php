b57e08541f720e4c6b7e230fd1547831f0017c30
Matthew <realazurenova@gmail.com>
2026-02-24 09:57:43 +0100

    Idk

--- DU2/comps/database.php
+++ DU2/comps/database.php
@@ -1,12 +1,12 @@
 <?php
-$dsn = "mysql:host=127.0.0.1;dbname=testdb;charset=utf8mb4";
-$username = "testadmin";
-$password = "adminadmin";
+    $dsn = "mysql:host=127.0.0.1;dbname=testdb;charset=utf8mb4";
+    $username = "testadmin";
+    $password = "adminadmin";
 
-try {
-    $db = new PDO($dsn, $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Success";
+    try {
+        $db = new PDO($dsn, $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Success";
     }
     catch (PDOException $e){
         echo "Unable to connect DB: ". $e->getMessage();
