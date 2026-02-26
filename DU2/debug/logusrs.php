83677ae2204b066a6f53545c87434dfba8578c42
Matthew <realazurenova@gmail.com>
2026-02-26 10:02:27 +0100

    Log users and permissions

--- DU2/debug/logusrs.php
+++ DU2/debug/logusrs.php
@@ -5,9 +5,17 @@
     echo "<h1>USER LOG</h1>";
     //check if logged in
     if (isset($_SESSION["username"]) && $_SESSION["isLoggedIn"] === 1){
+        $loggedUser = getbyUsername($_SESSION["username"]);
         $users = getALL("userdata");
         foreach($users as $user){
-            echo"<hr>".$user["id"]."<br>".$user["username"]."<br>".$user["usrpassword"]."<br>".$user["email"]."<hr>";
+            if($loggedUser["permissions"] === "admin"){
+                #echo "<img src='".$user["profileimg"]."' alt='pfp'";
+                echo"<hr>ID: ".$user["id"]."<br>USERNAME: ".$user["username"]."<br>PASSWORD: ".$user["usrpassword"]."<br>EMAIL: ".$user["email"]."<br>PERMISSIONS: ".$user["permissions"]."<hr>";
+            }
+            else{
+                #echo "<img src='".$user["profileimg"]."' alt='pfp'";
+            echo"<hr>ID: ".$user["id"]."<br>USERNAME: ".$user["username"]."<br>EMAIL: ".$user["email"]."<hr>";
+            }
         }
     }
     else{
